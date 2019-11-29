7a1c0699e6fc967f00a6280990e7ba38f4720c46
doutedasolla <doutedasolla@gmail.com>
2019-11-29 14:44:29 +0100

    Ordenador

---
+++ src/Computer.kt
@@ -0,0 +1,14 @@
+class Computer {
+    val marca:String
+    val pulgadas:Float
+
+    constructor(){
+        marca = "LG"
+        pulgadas = 17F
+    }
+
+    constructor(marca:String, pulgadas:Float){
+        this.marca=marca
+        this.pulgadas=pulgadas
+    }
+}
