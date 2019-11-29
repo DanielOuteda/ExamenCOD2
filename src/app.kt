43ceb7f56925eccf99d1ad466964919ffa32a3c0
doutedasolla <doutedasolla@gmail.com>
2019-11-29 14:38:00 +0100

    App principal

--- src/app.kt
+++ src/app.kt
@@ -1,12 +1,23 @@
 fun main() {
+    /*
+    Creo un nuevo repositorio en github llamado ExamenCOD2
+    Utilizo la terminal en el lugar donde se encuetra el documento
+    utilizo git init para inicializar un repositorio localmente
+    añado el repositorio usando git add src
+    hago distintos commits de cada clase con git commit -m "ejemplo"
+    subo todos los commits con git push -u src master
+    para crear las releases utilizo --tag
+    para dar permisos entro dentro del repositorio de github/ configuracion collaborators u escribo el nombre de usuario de lreceptor
+
+     */
     println("Esto es una app")
-    var mouseA = Mouse()
+    var mouseA = CPU()
     println(mouseA.tipo)
-    var mouseB = Mouse("Inalambrico")
+    var mouseB = CPU("Inalambrico")
     println(mouseB.tipo)
     mouseA.tipo="bluetooth"
     println(mouseA.tipo)
-    var mipantalla = Screen()
+    var mipantalla = Computer()
     println(mipantalla.marca)
 
 }
